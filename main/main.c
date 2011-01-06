82a4424750cc6c9dec14d4c7c6fd6e749e6da31a
peter <username@mydomain.com>
2011-01-06 20:29:30 +0800

    11.1.6

--- main/main.c
+++ main/main.c
@@ -3,15 +3,20 @@ float cal_money(float distance,long wait_time, int day)
 {
     float money;
     wait_time = wait_time / 300;
+    printf("%ld\n",wait_time );
     distance = distance + wait_time;
     if(day)
     {
        money = distance < 3 ? 10 : (distance - 3) * 2 + 10 ; 
+       if(distance > 15)
+       money = money + (distance - 15) * 1;
        return money;
     }
     else
     {
        money = distance < 3 ? 11 : (distance - 3) * 2.4 + 11 ; 
+       if(distance > 15)
+           money = money + (distance - 15) * 1.2;
        return money;
     }
 
@@ -19,7 +24,7 @@ float cal_money(float distance,long wait_time, int day)
 int main(int argc, const char *argv[])
 {
    float money, diatance, time;
-   money = cal_money(6,620,0);
+   money = cal_money(16,20,1);
    printf("%1.2f\n",money);
     
     return 0;
